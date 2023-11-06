03caf4b055f5268887594ceb1018902659e8147e
Certems <james.lostarkgames@gmail.com>
2023-11-06 18:13:55 +0000

    fixes

--- default/behaviour_funDudes.js
+++ default/behaviour_funDudes.js
@@ -1,15 +1,14 @@
 var funTask = {
     doFun : function(creep){
         //Run to other base and spam words
-        if(creep.memory.isTrolling){
-            var targetLocation;
-            if(creep.room.roomName == E53N22){
-                targetLocation = Game.map.findRoute(E53N22, E53N21);}
-            else{
-                targetLocation = new RoomPosition(20,20);}
-            creep.moveTo(targetLocation);
-            creep.say('🤡🤡🤡🤡🤡');
-        }
+        var targetLocation;
+        if(creep.room.name == "E53N22"){
+            targetLocation = new RoomPosition(6,49,creep.room.name);}
+        else{
+            targetLocation = creep.room.spawns["spawn0"];}
+        console.log(targetLocation);
+        creep.moveTo(targetLocation);
+        creep.say('🤡🤡🤡🤡🤡');
     },
     respawn : function(relatedCreepNumber){
         if(relatedCreepNumber < 1){
