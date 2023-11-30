46cb2be4465a0751320896e4d148984782a73ee8
Certems <james.lostarkgames@gmail.com>
2023-11-30 21:15:21 +0000

    multi-room building

--- default/behaviour_Builder.js
+++ default/behaviour_Builder.js
@@ -2,38 +2,74 @@ var {getSpawnerRoomIndex} = require("manager_Memory");
 
 var building_tasks = {
     task : function(creep){
-        if(creep.memory.isBuilding){
-            if(creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0){
-                var towersNeedingSupply = creep.room.find(FIND_STRUCTURES, {filter:(structure) => {return ( (structure.structureType == STRUCTURE_TOWER) && (structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) )}});
-                var towersSupplied      = (towersNeedingSupply.length == 0);
-                if(towersSupplied){
-                    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-                    if(targets.length > 0){
-                        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE){
-                            creep.moveTo(targets[0]);
-                        }
+        var target = getTarget_builder(creep);
+        if(target){
+            if(creep.memory.isBuilding){
+                var inCorrectRoom = creep.room.name == target.room.name;
+                if(inCorrectRoom){
+                    if(creep.memory.travelRoute){
+                        delete creep.memory.travelRoute;}
+                    //In correct room, therefore mine to the source you are here for
+                    if(target.structureType == STRUCTURE_TOWER){    //(1) Refill towers as a priority
+                        if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                            creep.moveTo(target);}
+                    }
+                    else{                                           //(2) Construct within this spawner's influence (e.g associated energy rooms)
+                        if(creep.build(target) == ERR_NOT_IN_RANGE){
+                            creep.moveTo(target);}
                     }
                 }
                 else{
-                    var target = creep.pos.findClosestByPath(towersNeedingSupply);
-                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                        creep.moveTo(target);
+                    //Not in correct room, therefore path to this room
+                    if(!creep.memory.travelRoute){   //Deleted once it is no longer in use
+                        //Generate route to the final room
+                        creep.memory.travelRoute = Game.map.findRoute(creep.room.name, target.room.name);}
+                    else{
+                        //Follow the route to the final room
+                        if(creep.room.name == creep.memory.travelRoute[0].room){    //If in next room, remove it to mark it as travelled
+                            creep.memory.travelRoute.shift();}
+                        if(creep.memory.travelRoute.length > 0){
+                            creep.moveTo(creep.pos.findClosestByPath(creep.room.find(creep.memory.travelRoute[0].exit)));}
+                        //#############################################################################################
+                        //## MAY BREAK AS I ENTER THE FINAL ROOM HERE, HE MAY PAUSE FOR 2 FRAMES BEFORE PROPER GOING ##
+                        //#############################################################################################
                     }
                 }
                 //## MAYBE MOVE CREEPS TO A SAFE FLAG SPOT IF NO SITES NEED TO BE MADE ##
+                if(creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0){
+                    creep.memory.isBuilding = false;
+                }
             }
             else{
-                creep.memory.isBuilding = false;
-            }
-        }
-        else{
-            //Prepare for construction
-            var spawn = Game.getObjectById(creep.memory.spawnKey.spawnID);
-            if(creep.withdraw(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                creep.moveTo(spawn);
-            }
-            if(creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0){
-                creep.memory.isBuilding = true;
+                //Prepare for construction
+                var inCorrectRoom = creep.room.name == creep.memory.spawnKey.roomID;
+                if(inCorrectRoom){
+                    if(creep.memory.travelRoute){
+                        delete creep.memory.travelRoute;}
+                    //In correct room, therefor withdraw required
+                    if(creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                        creep.moveTo(target);
+                    }
+                }
+                else{
+                    //Not in correct room, therefore path to this room
+                    if(!creep.memory.travelRoute){   //Deleted once it is no longer in use
+                        //Generate route to the final room
+                        creep.memory.travelRoute = Game.map.findRoute(creep.room.name, creep.memory.spawnKey.roomID);}
+                    else{
+                        //Follow the route to the final room
+                        if(creep.room.name == creep.memory.travelRoute[0].room){    //If in next room, remove it to mark it as travelled
+                            creep.memory.travelRoute.shift();}
+                        if(creep.memory.travelRoute.length > 0){
+                            creep.moveTo(creep.pos.findClosestByPath(creep.room.find(creep.memory.travelRoute[0].exit)));}
+                        //#############################################################################################
+                        //## MAY BREAK AS I ENTER THE FINAL ROOM HERE, HE MAY PAUSE FOR 2 FRAMES BEFORE PROPER GOING ##
+                        //#############################################################################################
+                    }
+                }
+                if(creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0){
+                    creep.memory.isBuilding = true;
+                }
             }
         }
     },
@@ -63,6 +99,55 @@ var building_tasks = {
         1. ...
         */
     }
+}
+
+function getTarget_builder(creep){
+    /*
+    Finds targets for this builder to construct
+
+    resupplyTowers = towers that NEED a resupply
+    */
+    var towersNeedingSupply = creep.room.find(FIND_STRUCTURES, {filter:(structure) => {return ( (structure.structureType == STRUCTURE_TOWER) && (structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) )}});
+    var towersSupplied      = (towersNeedingSupply.length == 0);
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    var target = creep.pos.findClosestByPath(towersNeedingSupply);
+    var spawn = Game.getObjectById(creep.memory.spawnKey.spawnID);
+
+
+    var target = null;
+    if(creep.memory.isBuilding){
+        //Look for (1) Towers to refill, (2) Construction sites within influence of spawner
+        var resupplyTowers = Game.rooms[creep.memory.spawnKey.roomID].find(FIND_STRUCTURES, {filter:(structure) => {return ( (structure.structureType == STRUCTURE_TOWER) && (structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) )}});
+        if(resupplyTowers.length > 0){  //(1)
+            target = creep.pos.findClosestByPath(resupplyTowers);
+        }
+        else{                           //(2)
+            for(var roomIndex in Memory.energyRooms){
+                if(Memory.energyRooms[roomIndex].spawnerRoomID == creep.memory.spawnKey.roomID){    //If this energy room is associated with this builder's spawn, check if he has any jobs he can do there
+                    if(Game.rooms[Memory.energyRooms[roomIndex].spawnerRoomID]){   //If you currently have vision of that room (this will change as creeps enter, leave and die in a room)
+                        var constructSites = Game.rooms[Memory.energyRooms[roomIndex].spawnerRoomID].find(FIND_CONSTRUCTION_SITES);
+                        if(constructSites.length > 0){
+                            target = constructSites[0]; //May be in other rooms, so cant simply check path diff => just complete them in order (fine assumption because 90% of time nothing will require building)
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else{
+        //For look place to gather resources from in spawner room
+        if(creep.room.name == creep.memory.spawnKey.roomID){
+            //If in spawner room, look for closest storage (extension or spawner)
+            possibleTargets = creep.room.find(FIND_STRUCTURES, {filter:(structure) => {return( ((structure.structureType = STRUCTURE_SPAWN)&&(structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0)) || ((structure.structureType = STRUCTURE_EXTENSION)&&(structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0)) )}});
+            target = creep.pos.findClosestByPath(possibleTargets);
+        }
+        else{
+            //If NOT in home spawner room, just travel to the spawner generally, re-adjust later
+            target = Game.rooms[creep.memory.spawnKey.roomID].find(FIND_STRUCTURES, {filter:(structure) => {return(structure.structureType = STRUCTURE_SPAWN)}})[0];
+        }
+    }
+    return target;
 }
 
 module.exports = building_tasks;
