b4d9cc4e74563ddcd0792bac6c210a905c83237a
Certems <james.lostarkgames@gmail.com>
2023-12-03 13:37:24 +0000

    fix

--- default/manager_Market.js
+++ default/manager_Market.js
@@ -5,11 +5,7 @@ function calcMarket_general(){
     var transactionCost = Game.market.calcTransactionCost(1000, "E53N22", "E44N1");
     console.log("TransactionCost; ",transactionCost);
 }
-function other(){
-    //pass
-}
 
 module.exports = {
-    calcMarket_general,
-    other
+    calcMarket_general
 };
