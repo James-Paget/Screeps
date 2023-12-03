651dede467fb115f8c208e23f7793f557a7a6a50
Certems <james.lostarkgames@gmail.com>
2023-12-03 14:00:52 +0000

    changes

--- default/manager_Market.js
+++ default/manager_Market.js
@@ -2,11 +2,19 @@ function calcMarket_general(){
     /*
     Just a test function to get a feel for how transactions should occur
     */
-    var transactionCost = Game.market.calcTransactionCost(1000, "E53N22", "E59N29");
-    console.log("TransactionCost; ",transactionCost);
-    //E59N29
-    //656c846f77f75f001288d1a8
-    Game.market.deal(orderId, amount, [yourRoomName])
+    var deal_room_name = "";    //Room name related to order
+    var deal_ID        = "";    //Order ID on the market
+    var deal_amount    = 0;     //Sell amount to order
+    var deal_sell_cost = Game.market.calcTransactionCost(deal_amount, "E53N22", "deal_room_name");
+    
+    //console.log("---");
+    //console.log("TransactionCost; ",deal_sell_cost);
+    //console.log("cTime -> ",Game.time);
+    
+    if(Game.time == 0){
+        console.log("### Deal Executed ###");
+        Game.market.deal(deal_ID, deal_amount, "E53N22");
+    }
 }
 
 module.exports = {
