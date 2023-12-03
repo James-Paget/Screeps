ee093b664f4f48175131d1560054e34752ecd1d1
Certems <james.lostarkgames@gmail.com>
2023-12-03 13:48:14 +0000

    market price

--- default/manager_Market.js
+++ default/manager_Market.js
@@ -2,8 +2,11 @@ function calcMarket_general(){
     /*
     Just a test function to get a feel for how transactions should occur
     */
-    var transactionCost = Game.market.calcTransactionCost(1000, "E53N22", "E44N1");
+    var transactionCost = Game.market.calcTransactionCost(1000, "E53N22", "E59N29");
     console.log("TransactionCost; ",transactionCost);
+    //E59N29
+    //656c846f77f75f001288d1a8
+    Game.market.deal(orderId, amount, [yourRoomName])
 }
 
 module.exports = {
