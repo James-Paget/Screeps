dff04697d6bc4c0cc33b5b7ca2b7f75c95dc8fa0
Certems <james.lostarkgames@gmail.com>
2023-12-16 12:32:42 +0000

    fixes

--- default/behaviour_Tower.js
+++ default/behaviour_Tower.js
@@ -4,7 +4,7 @@ var tower_tasks = {
         if(isInvasion){
             towerAttack_hostileCreeps(tower);}
         else{
-            if(tower.room.find(FIND_CONSTRUCTION_SITES).length == 0){
+            if(tower.room.find(FIND_CONSTRUCTION_SITES).length == 0){   //############# REMAKE THIS, HAVE IT EITHER (1) REPAIR ESSENTIAL THINGS LIKE TOWERS, CONTAINERS, RAMPARTS WHILE CONSTRUCTION OCCURS OR (2) REPAIR WALLS & RAMPARTS FULLY WHEN EXCESS
                 if(tower.store.getUsedCapacity(RESOURCE_ENERGY) >= 0.5*tower.store.getCapacity(RESOURCE_ENERGY)){  //Any repair when over half energy, in case of attack
                     towerRepair_prioirity(tower);}
                 }
@@ -36,7 +36,7 @@ function towerRepair_prioirity(tower){
     if(targetsPrio.length == 0){
         targetsPrio = tower.room.find(FIND_STRUCTURES, {filter : (structure) => {return ( (structure.structureType == STRUCTURE_TOWER) && (structure.hits < structure.hitsMax*0.8) )}});
         if(targetsPrio.length == 0){
-            targetsPrio = tower.room.find(FIND_STRUCTURES, {filter : (structure) => {return ( (structure.structureType == STRUCTURE_RAMPART) && (structure.hits < structure.hitsMax*0.8) )}});  //Prio. Rampart over walls as ramparts can decay to destruction, walls cant
+            targetsPrio = tower.room.find(FIND_STRUCTURES, {filter : (structure) => {return ( (structure.structureType == STRUCTURE_RAMPART) && (structure.hits < structure.hitsMax*0.0001) )}});  //Prio. Rampart over walls as ramparts can decay to destruction, walls cant
             if(targetsPrio.length == 0){
                 targetsPrio = tower.room.find(FIND_STRUCTURES, {filter : (structure) => {return ( (structure.structureType != STRUCTURE_WALL) && (structure.hits < structure.hitsMax*0.8) )}});
                 if(targetsPrio.length == 0){
