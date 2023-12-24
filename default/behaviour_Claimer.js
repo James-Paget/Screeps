ed0979628cacac328504c49f241fed8cf4d25ba7
James <james.lostarkgames@gmail.com>
2023-12-24 22:44:52 +0000

    notes

---
+++ default/behaviour_Claimer.js
@@ -0,0 +1,12 @@
+var claimer_tasks = {
+    task : function(){
+        //pass
+    }
+}
+/*
+- Claimers should be auto-sent to specified locations
+- The rate of claimers sent should be changable
+
+--- NEED SOME SCAVENGERS TO LOOT OUTPOSTS ---
+*/
+module.exports = claimer_tasks;
--- default/behaviour_Military.js
+++ default/behaviour_Military.js
@@ -12,5 +12,20 @@ Maybe split into multiple var sections;
 - Militia Attack
 
 --> So different behaviours can be switched between and triggered at an instant
+
+WHAT I WANT;
+- NEED a function to let me call an [arbitrary attack] of [arbitrary size] to 
+an [arbitrary map], with [arbitrary targets] maybe
+- Their brains should work the same, but just follow their given priorities
+- Call a function from miltary tasks to queue up a set number of creeps 
+from a given spawner, that will head towards a given map , an attack enemies 
+in that area
+    --> This attacking should occur in a given priority, parsed into the creeps memory, 
+    so they can each remember their own orders (e.g attack creeps, then core, then ... 
+    OR heal this unit, then this unit, then ...)
+- Each function should queue up a different set of fighters to do different roles
+- Each should be very reactive
+- There should be functions to auto-spawn militia if invader cores OR enemies are detected 
+at the spawnerRoom or linked energyRooms
 */
 module.exports = military_tasks;
