0cd345a53d32cca33609689d50fb2085c001a7b2
James <james.lostarkgames@gmail.com>
2023-12-27 23:42:52 +0000

    market fixed

--- default/manager_Market.js
+++ default/manager_Market.js
@@ -2,10 +2,11 @@ function calcMarket_general(){
     /*
     Just a test function to get a feel for how transactions should occur
     */
+    var my_room_name   = "";    //Room you are in
     var deal_room_name = "";    //Room name related to order
     var deal_ID        = "";    //Order ID on the market
     var deal_amount    = 0;     //Sell amount to order
-    var deal_sell_cost = Game.market.calcTransactionCost(deal_amount, "E53N22", "deal_room_name");
+    var deal_sell_cost = Game.market.calcTransactionCost(deal_amount, my_room_name, deal_room_name);
     
     //console.log("---");
     //console.log("TransactionCost; ",deal_sell_cost);
@@ -13,7 +14,7 @@ function calcMarket_general(){
     
     if(Game.time == 0){
         console.log("### Deal Executed ###");
-        Game.market.deal(deal_ID, deal_amount, "E53N22");
+        Game.market.deal(deal_ID, deal_amount, my_room_name);
     }
 }
 
