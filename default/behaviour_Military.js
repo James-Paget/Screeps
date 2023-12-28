30f8c2a83cc751ae06ccf23e1d3063305ccbbf22
James <james.lostarkgames@gmail.com>
2023-12-28 00:25:53 +0000

    Military progression

--- default/behaviour_Military.js
+++ default/behaviour_Military.js
@@ -1,6 +1,26 @@
+//################################################
+//################################################
+//## NEED TO MINIMISE NUMBER OF CREEPS BEING USED --> MAKE LARGER ONES MORE OFTEN
+//################################################
+//################################################
 var military_tasks = {
     task : function(){
-        //pass
+        /*
+        1. Go through the creeps priorities
+        2. Keep looking until you reach a task that hasnt been satisfied
+        3. Do that action
+
+        Note; Priority 0 will almost always be travel to the target location, 
+            OR maybe attack-move to the target room
+        */
+        //1
+        for(var prioIndex in creep.memory.jobOrder.priority){
+            var isPrioSatisfied = checkPrioritySatisfied(creep, creep.memory.jobOrder.priority[prioIndex]);
+            if(!isPrioSatisfied){
+                //3
+                performPriority(creep, creep.memory.jobOrder.priority[prioIndex]);
+            }
+        }
     },
     generateCreepParts : function(){
         //--> Maybe;
@@ -17,7 +37,7 @@ var military_tasks = {
         var spawner   = Game.getObjectById(spawnerID);
         var houseKey  = {roomID:creepSpec.roomID , sourceID:creepSpec.sourceID};    //Their target room info
         var spawnKey  = {roomID:spawner.room.name, spawnID:spawnerID};              //Their spawner info
-        spawner.spawnCreep(creepSpec.parts, creepName, {memory:{role:creepSpec.role, spawnKey:spawnKey, houseKey:houseKey, priority:creepSpec.priority}});
+        spawner.spawnCreep(creepSpec.parts, creepName, {memory:{role:creepSpec.role, spawnKey:spawnKey, houseKey:houseKey, jobOrder:creepSpec.jobOrder}});
     },
     death : function(){
         /*
@@ -29,61 +49,98 @@ var military_tasks = {
     }
 }
 
-function generate_militia(spawnerID, markID, lvl){
-    /*
-    This can be called at any time to queue up a militia squad;
-    -From a given spawner, spawnerID
-    -Attacking a given room, markID
-    -Of a given power level, lvl
-
-    --> Job order contains target priorities, markID, ...
-    */
-    //###########
-    //## EACH MILITIA MEMBER SHOULD ESSENTIALLY WORK AUTONOMOUSLY, BUT MAY 
-    //## HAVE SOME FEATURES THAT ALLOW THEM TO WAIT FOR EACH OTHER
-    //##    --> EACH SHOULD REMEMBER KEY FACTS
-    //###########
-    var creepNumber = 1*lvl;
-    for(var i=0; i<creepNumber; i++){
-        var jobOrder = generate_preset_jobOrder(markID);
-        var parts    = generate_creepParts();
-        military_tasks.queue(spawnerRoomID, null, parts, jobOrder);
-    }
-}
-//A simple function for each required military group
-//...
-
-function generate_preset_jobOrder(){
-    //pass
-    return null;
-}
-function generate_creepParts(){
-    //pass
-    return null;
-}
+//----------------
+//Military Groups
+//----------------
 /*
-Maybe split into multiple var sections;
 - Army Guard
 - Post Guard
 - Army Attack
 - Post Attack
 - Militia Attack
-
 --> So different behaviours can be switched between and triggered at an instant
-
-WHAT I WANT;
-- NEED a function to let me call an [arbitrary attack] of [arbitrary size] to 
-an [arbitrary map], with [arbitrary targets] maybe
-- Their brains should work the same, but just follow their given priorities
-- Call a function from miltary tasks to queue up a set number of creeps 
-from a given spawner, that will head towards a given map , an attack enemies 
-in that area
-    --> This attacking should occur in a given priority, parsed into the creeps memory, 
-    so they can each remember their own orders (e.g attack creeps, then core, then ... 
-    OR heal this unit, then this unit, then ...)
-- Each function should queue up a different set of fighters to do different roles
-- Each should be very reactive
-- There should be functions to auto-spawn militia if invader cores OR enemies are detected 
-at the spawnerRoom or linked energyRooms
 */
+function generate_militia(lvl, spawnerID, targetRoomID){
+    /*
+    #####################################################################
+    ## THEY SHOULD SAY ABOVE THEM PERIODICALLY WHAT TYPE OF UNIT THEY ARE 
+    ## e.g Skull&CrossBones for militia, ...
+    #####################################################################
+
+    This can be called at any time to queue up a militia squad;
+    -From a given spawner, spawnerID
+    -Attacking a given room, markID
+    -Of a given power level, lvl
+
+    Militia work as follows;
+    (0) Wait for other 'squad members'
+    (1) Move to the target room (soft-like)
+    (2) Attempt to kill any hostile creeps in that room
+    (3) Attempt to destroy any hostile towers
+    (4) Attempt to destroy any "Invader Cores" or similar
+    */
+    var creepNumber = 1*lvl;
+    for(var i=0; i<creepNumber; i++){
+        var jobOrder = null;        //#### FIGURE THIS OUT ####
+        var parts    = null;        //#### FIGURE THIS OUT ####
+        var squadID  = null;    //Used to group squads
+        //###################################
+        //MAKE A PART OF THEIR NAME #########
+        //###################################
+        military_tasks.queue(spawnerRoomID, null, parts, jobOrder);
+    }
+}
+//...
+
+//--------------------
+//Priority Management
+//--------------------
+function checkPrioritySatisfied(creep, priority){
+    /*
+    Checks if a given priority is satisfied
+
+    As more priority types are added, extend this list
+    */
+    var isSatisfied = null;
+    if      (priority.name == "moveToTargetSoft"){
+        checkPrioritySatisfied_moveToTargetSoft(creep, priority);}
+    else if (priority.name == "..."){
+        //pass
+    }
+    //...
+    return isSatisfied; //null OR true => ignore and move onto next priority
+}
+function performPriority(creep, priority){
+    /*
+    Complete the given priority (/take a step towards completing)
+
+    As more priority types are added, extend this list
+    */
+    var isSatisfied = null;
+    if      (priority.name == "moveToTargetSoft"){
+        performPriority_moveToTargetSoft(creep, priority);}
+    else if (priority.name == "..."){
+        //...
+    }
+    //...
+    return isSatisfied; //null OR true => ignore and move onto next priority
+}
+
+//-------------------
+//Priority Specifics
+//-------------------
+function checkPrioritySatisfied_moveToTargetSoft(creep, priority){
+    /*
+    Checks if the creep is currently in the required room / exact location (which 
+    can be specified through an optional arguement, by changing from null)
+    */
+    //pass
+}
+function performPriority_moveToTargetSoft(creep, priority){
+    /*
+    Move to room target WITHOUT attacking on the way
+    */
+    //pass
+}
+
 module.exports = military_tasks;
